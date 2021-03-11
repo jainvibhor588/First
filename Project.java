77b715a45cee1bdf180657e1024e55b3c91f71cb
jainvibhor588 <48271798+jainvibhor588@users.noreply.github.com>
2021-03-11 20:51:36 +0530

    Add files via upload

---
+++ Project.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Project{
+public static void main(String... args)
+{
+Scanner sc=new scanner(System.in);
+int task=sc.nextInt();
+int ind=sc.nextInt();
+int days=sc.nextInt();
+if(task<(ind*days))
+{
+System.out.println("1");
+
+}
+else
+{
+System.out.println("0");
+}
+
+}
+
+}
